c46a65c3691dd1c8e3a4b832e15ff9ebecf655b3
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-27 09:48:57 +0600

    adverside page data loading depending on sale status and advertised, favicon added

--- src/Pages/Home/AdvertiseProducts/AdvertiseProducts.js
+++ src/Pages/Home/AdvertiseProducts/AdvertiseProducts.js
@@ -5,7 +5,7 @@ import BookingModal from '../../BookingModal/BookingModal';
 
 const AdvertiseProducts = () => {
 
-    const[productInfo,setProductInfo]=useState(null)
+    const [productInfo, setProductInfo] = useState(null)
 
     const { data: advertised = [], refetch } = useQuery({
 
@@ -23,35 +23,38 @@ const AdvertiseProducts = () => {
             return data;
         }
     })
-
+    
 
     return (
         <div className='mt-14'>
 
-            <h2 className='text-xl font-bold text-center'>Advertised products are</h2>
+            {
+                advertised?.length && 
+                <h2 className='text-xl font-bold text-center'>Advertised products are</h2>
+            }
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-12 mb-16 px-4'>
                 {
                     advertised.map(product => <ProductCard
                         key={product._id}
                         product={product}
-                         setProductInfo={setProductInfo}
+                        setProductInfo={setProductInfo}
                     >
 
                     </ProductCard>)
-                    
+
                 }
             </div>
 
-           {
-            productInfo &&
-             <BookingModal
-             product={productInfo}
-             setProductInfo={setProductInfo}
-             >
- 
-             </BookingModal>
-           }
+            {
+                productInfo &&
+                <BookingModal
+                    product={productInfo}
+                    setProductInfo={setProductInfo}
+                >
+
+                </BookingModal>
+            }
 
         </div>
     );
