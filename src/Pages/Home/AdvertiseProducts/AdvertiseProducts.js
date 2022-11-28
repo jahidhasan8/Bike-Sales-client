6f7df5572aafd6a8085e3013ebc2ced2e38cdf90
jahidhasan8 <jahidhasan1932@gmail.com>
2022-11-28 22:04:30 +0600

    advertiseProducts page modified

--- src/Pages/Home/AdvertiseProducts/AdvertiseProducts.js
+++ src/Pages/Home/AdvertiseProducts/AdvertiseProducts.js
@@ -1,14 +1,12 @@
-import React, { useContext, useState } from 'react';
+import React, {  useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import ProductCard from '../../ProductCategories/ProductCard/ProductCard';
 import BookingModal from '../../BookingModal/BookingModal';
-import { AuthContext } from '../../../contexts/AuthProvider';
 import Loader from '../../Shared/Loader/Loader';
 
 const AdvertiseProducts = () => {
 
     const [productInfo, setProductInfo] = useState(null)
-    const{user}=useContext(AuthContext)
 
     const { data: advertised = [],isLoading } = useQuery({
 
@@ -18,9 +16,6 @@ const AdvertiseProducts = () => {
 
             const res = await fetch(`http://localhost:5000/products/advertised`, {
 
-                // headers: {
-                //     authorization: `bearer ${localStorage.getItem('jwToken')}`
-                // }
             });
             const data = await res.json();
             return data;
@@ -33,12 +28,12 @@ const AdvertiseProducts = () => {
     }
 
   
-    // advertised.length && 
+    
     return (
         <div className='mt-14'>
 
             {
-                user?.email &&
+                
                 advertised.length &&
                 <h2 className='text-xl font-bold text-center'>Advertised products are</h2>
                 
@@ -49,7 +44,7 @@ const AdvertiseProducts = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-12 mb-16 px-4'>
                 {
-                     user?.email &&
+                    
                     advertised?.map(product => <ProductCard
                         key={product._id}
                         product={product}
