390e2a36376018e5d8a3105909bee5090413411e
jahidhasan8 <jahidhasan1932@gmail.com>
2023-02-03 01:29:20 +0600

    category modified

--- src/Pages/ProductCategories/ProductCategories/ProductCategories.js
+++ src/Pages/ProductCategories/ProductCategories/ProductCategories.js
@@ -33,7 +33,17 @@ const ProductCategories = () => {
                 {
                     categories.map(category => <div className='text-center border bg-info' key={category._id}>
 
-                        <p className='p-2 font-bold text-2xl text-center text-white' ><Link to={`/products/${category._id}`}>{category.name}</Link></p>
+                    <Link to={`/products/${category._id}`}>
+            <div className="card h-32  items-center card-side cursor-pointer shadow-xl px-5 py-5 hover:-translate-y-1 hover:scale-110 hover:bg-cyan-400 flex justify-between">
+                <div>
+                    <figure><img src={category.image} alt="bikeimage" className='w-24' /></figure>
+                </div>
+                <div className="card-body text-white">
+                    <h2 className="card-title font-bold text-3xl">{category.name}</h2>
+                </div>
+            </div>
+        </Link>
+
                     </div>)
                 }
 
